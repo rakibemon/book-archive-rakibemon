68cdda8835feecca953e7f7d864db63cee601419
rakibemon <rakibemon03@gmail.com>
2021-09-02 23:02:00 +0600

    final commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const inputField = document.getElementById('input-text');
-const inputValue=inputField.value;
+// add event handler on search button
 document.getElementById('search-button').addEventListener('click', () => {
+    // make display clear on every search
     document.getElementById("books-container").textContent = "";
     document.getElementById("empty-error").textContent = "";
-    document.getElementById('books-counter').textContent=''
+    document.getElementById('books-counter').textContent = ''
     document.getElementById("error-message").classList.add('d-none');
     const emptyError = document.getElementById('empty-error');
     if (inputField.value.length > 0) {
@@ -22,43 +23,43 @@ document.getElementById('search-button').addEventListener('click', () => {
 
     inputField.value = ''
 });
+// function to display books card
 const displayBook = (books) => {
     const booksContainer = document.getElementById('books-container');
-    const h4=document.createElement('h4');
-        h4.innerHTML=` Showing result of - ${books.length}`
-        h4.classList.add('text-center');
-        document.getElementById('books-counter').appendChild(h4)
-    if(books.length==0){
+    const h4 = document.createElement('h4');
+    h4.innerHTML = ` Showing result of - ${books.length}`
+    h4.classList.add('text-center', 'mt-4');
+    document.getElementById('books-counter').appendChild(h4)
+    if (books.length === 0) {
         errorMessage()
     }
-   else{
-    books.forEach(book => {
-        document.getElementById("error-message").classList.add('d-none')
-        const { cover_i, title, author_name, first_publish_year, publisher} = book;
-        console.log(books)
-        const div = document.createElement('div')
-        div.classList.add('col');
-        div.innerHTML = `
+    else {
+        books.forEach(book => {
+            document.getElementById("error-message").classList.add('d-none')
+            const { cover_i, title, author_name, first_publish_year, publisher } = book;
+            const div = document.createElement('div')
+            div.classList.add('col');
+            div.innerHTML = `
         <div class="card h-100">
-    <img src="https://covers.openlibrary.org/b/id/${cover_i}-M.jpg" class="card-img-top text-center img-thumbnail" alt="Book Image">
+    <figure> <img src="https://covers.openlibrary.org/b/id/${cover_i ? cover_i:10909258}-M.jpg" class="card-img-top text-center img-thumbnail img-fluid" alt="Book Image"></figure>
     <div class="card-body">
-      <h5 class="card-title">${title}</h5>
-      <p> <b>Author Name :</b> ${author_name}</p>
-      <p> <b>First publish Year :</b> ${first_publish_year}</p>
-      <p> <b>Publisher : </b> ${publisher}</p>
+      <h5 class="card-title">${title ? title : 'no data found'}</h5>
+      <p> <b>Author Name :</b> ${author_name ? author_name : "No author name found"}</p>
+      <p> <b>First publish Year :</b> ${first_publish_year ? first_publish_year : 'no data found'}</p>
+      <p> <b>Publisher : </b> ${publisher ? publisher : 'no data found'}</p>
     </div>
   </div>`
-        booksContainer.appendChild(div)
-    })
-   }
+
+            booksContainer.appendChild(div)
+        })
+    }
     document.getElementById('spinner').classList.add('d-none');
 
 
 }
-
+// function to show if search named book ain't available
 const errorMessage = () => {
     const errorMessage = document.getElementById("error-message");
-    // document.getElementById("books-container").textContent = "";
     errorMessage.innerHTML = ` <div class="card m-auto p-5 bg-danger text-white" style="width: 18rem">
             <h5 class="card-title">Dear Sir/Ma'am,</h5>
             <p class="card-text">
